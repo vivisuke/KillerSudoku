6eda9b8445958ab60f3e7783af30665aca663edf
vivisuke <ntsuda@master.email.ne.jp>
2022-01-28 10:26:09 +0900

    0011 ◎ C 01/18 01/28 0.001│ToDo│解答生成 0012 ◎ C 01/18 01/28 0.001│ToDo│入力数字表示用 InputLabel.tscn 追加

--- KillerSudoku/scripts/MainScene.gd
+++ KillerSudoku/scripts/MainScene.gd
@@ -19,26 +19,117 @@ const ALL_BITS = (1<<N_HORZ) - 1
 var cage_labels = []		# ケージ合計数字用ラベル配列
 var clue_labels = []		# 手がかり数字用ラベル配列
 var input_labels = []		# 入力数字用ラベル配列
+var ans_bit = []			# 解答の各セル数値（0 | BIT_1 | BIT_2 | ... | BIT_9）
+var cell_bit = []			# 各セル数値（0 | BIT_1 | BIT_2 | ... | BIT_9）
+var candidates_bit = []		# 入力可能ビット論理和
+var column_used = []		# 各カラムの使用済みビット
+var box_used = []			# 各3x3ブロックの使用済みビット
 
 var CageLabel = load("res://CageLabel.tscn")
 var ClueLabel = load("res://ClueLabel.tscn")
+var InputLabel = load("res://InputLabel.tscn")
 
 func _ready():
+	cell_bit.resize(N_CELLS)
+	candidates_bit.resize(N_CELLS)
+	column_used.resize(N_HORZ)
+	box_used.resize(N_HORZ)
+	#
 	init_labels()
+	gen_ans()
+	for i in range(N_CELLS):
+		clue_labels[i].text = bit_to_numstr(cell_bit[i])
 	pass
+func xyToIX(x, y) -> int: return x + y * N_HORZ
+func num_to_bit(n : int): return 1 << (n-1) if n != 0 else 0
+func bit_to_num(b):
+	var mask = 1
+	for i in range(N_HORZ):
+		if (b & mask) != 0: return i + 1
+		mask <<= 1
+	return 0
+func bit_to_numstr(b):
+	if b == 0: return ""
+	return String(bit_to_num(b))
 func init_labels():
 	# 手がかり数字、入力数字用 Label 生成
 	for y in range(N_VERT):
 		for x in range(N_HORZ):
 			var px = x * CELL_WIDTH
 			var py = y * CELL_WIDTH
-			var label = ClueLabel.instance()
-			clue_labels.push_back(label)
-			label.rect_position = Vector2(px, py + 2)
-			label.text = String((x+y)%9 + 1)
-			$Board.add_child(label)
-			label = CageLabel.instance()
+			# ケージ合計用ラベル
+			var label = CageLabel.instance()
 			cage_labels.push_back(label)
 			label.rect_position = Vector2(px, py + 2)
 			label.text = "45"
 			$Board.add_child(label)
+			# 手がかり数字用ラベル
+			label = ClueLabel.instance()
+			clue_labels.push_back(label)
+			label.rect_position = Vector2(px, py + 2)
+			label.text = String((x+y)%9 + 1)
+			$Board.add_child(label)
+			# 入力数字用ラベル
+			label = InputLabel.instance()
+			input_labels.push_back(label)
+			label.rect_position = Vector2(px, py + 2)
+			label.text = ""
+			$Board.add_child(label)
+func gen_ans_sub(ix : int, line_used):
+	#print_cells()
+	#print_box_used()
+	var x : int = ix % N_HORZ
+	if x == 0: line_used = 0
+	var x3 = x / 3
+	var y3 = ix / (N_HORZ*3)
+	var bix = y3 * 3 + x3
+	var used = line_used | column_used[x] | box_used[bix]
+	if used == ALL_BITS: return false		# 全数字が使用済み
+	var lst = []
+	var mask = BIT_1
+	for i in range(N_HORZ):
+		if (used & mask) == 0: lst.push_back(mask)		# 数字未使用の場合
+		mask <<= 1
+	if ix == N_CELLS - 1:
+		cell_bit[ix] = lst[0]
+		return true
+	if lst.size() > 1: lst.shuffle()
+	for i in range(lst.size()):
+		cell_bit[ix] = lst[i]
+		column_used[x] |= lst[i]
+		box_used[bix] |= lst[i]
+		if gen_ans_sub(ix+1, line_used | lst[i]): return true
+		column_used[x] &= ~lst[i]
+		box_used[bix] &= ~lst[i]
+	cell_bit[ix] = 0
+	return false;
+
+func gen_ans():		# 解答生成
+	for i in range(N_CELLS):
+		clue_labels[i].text = "?"
+		input_labels[i].text = ""
+	for i in range(box_used.size()): box_used[i] = 0
+	for i in range(cell_bit.size()): cell_bit[i] = 0
+	var t = []
+	for i in range(N_HORZ): t.push_back(1<<i)
+	t.shuffle()
+	for i in range(N_HORZ):
+		cell_bit[i] = t[i]
+		column_used[i] = t[i]
+		box_used[i/3] |= t[i]
+	#print(cell_bit)
+	gen_ans_sub(N_HORZ, 0)
+	print_cells()
+	#update_cell_labels()
+	ans_bit = cell_bit.duplicate()
+	for i in range(N_CELLS): input_labels[i].text = ""		# 入力ラベル全消去
+	pass
+func print_cells():
+	var ix = 0
+	for y in range(N_VERT):
+		var lst = []
+		for x in range(N_HORZ):
+			lst.push_back(bit_to_num(cell_bit[ix]))
+			ix += 1
+		print(lst)
+	print("")
